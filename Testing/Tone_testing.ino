983a9d1d7589bf76fe093e87d32e34e4252fd9f9
Jonahw21 <jjwrubleski21@gmail.com>
2016-11-01 20:09:15 -0500

    Rename Tone_testing/Tone_testing.ino to Testing/Tone_testing.ino

---
+++ Testing/Tone_testing.ino
@@ -0,0 +1,25 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(9, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+tone(9, 523); //C
+delay(500);
+tone(9, 587); //D
+delay(500);
+tone(9, 659); //E
+delay(500);
+tone(9, 698); //F
+delay(500);
+tone(9, 784); //G
+delay(500);
+tone(9, 880); //A
+delay(500);
+tone(9, 988); //B
+delay(500);
+tone(9, 0);
+delay(500);
+}
