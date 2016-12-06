2b1d3287754140d3121041a855f7425aeeb253f4
Jonahw21 <jjwrubleski21@gmail.com>
2016-12-05 22:59:37 -0600

    Update wav_test.ino

--- Testing/wav_test.ino
+++ Testing/wav_test.ino
@@ -3,7 +3,9 @@
  * SD cards by default external modules can be connected using pins 13,12,11, and 4
  * It is also worth noting that a speaker will need to be connected to pin9 for
  * audio playback. If the card cannot be read or is improperly connected the
- * serial monitor will display "SD fail
+ * serial monitor will display "SD fail.
+ * The .wav file to be played should also be titled "test.wav" or the string in
+ * the tmrpcm.play() function call should be changed to the name of the .wav file
  */
 
 #include <SD.h>
